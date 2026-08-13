da34a9b180d6bf4f674b7c029cfd5c69b9e31c04
Sassone Ugo <cristiansassone22@gmail.com>
2026-08-13 18:46:40 +0200

    Aggiunge autosalvataggio bozza allenamento

---
+++ js/draft-manager.js
@@ -0,0 +1,89 @@
+
+function getDraftWorkout(){
+
+ return JSON.parse(
+  localStorage.getItem("draftWorkout") || "{}"
+ );
+
+}
+
+
+function saveDraftValue(day, exercise, set, type, value){
+
+ const draft=getDraftWorkout();
+
+ if(!draft[day]){
+  draft[day]={};
+ }
+
+ if(!draft[day][exercise]){
+  draft[day][exercise]={};
+ }
+
+ if(!draft[day][exercise][set]){
+  draft[day][exercise][set]={};
+ }
+
+ draft[day][exercise][set][type]=value;
+
+ localStorage.setItem(
+  "draftWorkout",
+  JSON.stringify(draft)
+ );
+
+}
+
+
+function getDraftValue(day, exercise, set, type){
+
+ const draft=getDraftWorkout();
+
+ return draft[day]?.[exercise]?.[set]?.[type] || "";
+
+}
+
+
+function attachDraftManager(){
+
+ document.querySelectorAll(".newSetRow input")
+ .forEach(input=>{
+
+  input.value =
+   getDraftValue(
+    currentDay,
+    input.dataset.exercise,
+    input.dataset.set,
+    input.dataset.type
+   );
+
+
+  input.addEventListener("input",()=>{
+
+   saveDraftValue(
+    currentDay,
+    input.dataset.exercise,
+    input.dataset.set,
+    input.dataset.type,
+    input.value
+   );
+
+  });
+
+ });
+
+}
+
+
+
+window.addEventListener("load",()=>{
+
+ setTimeout(()=>{
+
+  if(typeof attachDraftManager==="function"){
+   attachDraftManager();
+  }
+
+ },500);
+
+});
+
